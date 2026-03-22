a12ca7b9ea0f914531aadfeb62a899d00d9ebe4c
malachie45 <jeanledivinlowes@gmail.com>
2026-03-22 13:36:53 +0000

    mise à jour pages: home, about, sermons, events, contact

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from './components/navbar/navbar';
-import { Home } from './pages/home/home';
 import { Layout } from './layout/layout/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Layout],
+  imports: [Layout],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
